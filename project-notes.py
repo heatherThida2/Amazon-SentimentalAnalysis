ca3bc1c30f8e29febef4ea43c7beb891722e8b05
etrewitt <etrewitt@users.noreply.github.com>
2017-05-19 16:29:02 -0700

    update method

--- project-notes.py
+++ project-notes.py
@@ -13,24 +13,12 @@ jupyter notebook.
 """
 
 # Cell
-import gzip
 import pandas as pd
 
 # Cell
 # Helper methods provided by the review data source
-def parse(path):
-    g = gzip.open(path, 'r')
-    for l in g:
-        yield eval(l)
-
-def getDF(path):
-    i = 0
-    df = {}
-    for d in parse(path):
-        df[i] = d
-        i += 1
-    return pd.DataFrame.from_dict(df, orient='index')
 
 # Cell
-df = getDF('reviews_Video_Games_5.json.gz')
+vg = "reviews_Video_Games_5.json"
+df = pd.read_json(vg, lines=True)
 df.head()
