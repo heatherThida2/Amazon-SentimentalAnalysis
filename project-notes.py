3fb5f8422aa41bf9fa7c56ca862c2bdd14d4758c
etrewitt <etrewitt@users.noreply.github.com>
2017-05-19 18:04:04 -0700

    added 'good' column for log. regr.

--- project-notes.py
+++ project-notes.py
@@ -13,12 +13,30 @@ jupyter notebook.
 """
 
 # Cell
+import numpy as np
 import pandas as pd
+import sklearn as skl
 
 # Cell
-# Helper methods provided by the review data source
+train = pd.read_json("data/music_200.json", lines=True)
+test = pd.read_json("data/music_test_200.json", lines=True)
+train.head()
 
 # Cell
-vg = "reviews_Video_Games_5.json"
-df = pd.read_json(vg, lines=True)
-df.head()
+train = train.drop(['asin', 'helpful', 'reviewTime', 'reviewerID', 'reviewerName', 'summary', 'unixReviewTime'], axis=1)
+test = test.drop(['asin', 'helpful', 'reviewTime', 'reviewerID', 'reviewerName', 'summary', 'unixReviewTime'], axis=1)
+train.head()
+
+# Cell
+# train.assign(good = lambda g: g.overall >= 4)
+# train.assign(good = lambda g: (1, 0)[g.overall >= 4])
+train2 = train.assign(good = lambda g: g.overall >= 4)
+train.assign(good = train2['good'].apply(lambda g: 1 if g else 0))
+
+# Cell
+def rm_stopwords_punctuation(text):
+    with read("stopwords.json") as stopwords:
+        for word in stopwords:
+            text.replace(word, "")
+    # TODO: remove punctuation
+    return text
